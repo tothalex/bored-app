7580e0b683d3571bba837b8b787f6aa195d2023c
Toth Alex <tothalex097@gmail.com>
2020-04-20 22:52:35 +0200

    server route fixed

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const express = require('express')
 const serveStatic = require('serve-static')
 
 const app = express()
-app.use(serveStatic(`${__dirname}/dist`))
+
+const staticServe = serveStatic(`${__dirname}/dist`)
+
+app.use('/', staticServe)
+app.use('*', staticServe)
+
 const port = process.env.PORT || 5000
 app.listen(port)
 console.log(`server started ${port}`)
